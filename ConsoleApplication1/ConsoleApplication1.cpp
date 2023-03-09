9d9807ef55c409e7703a06a52636f2af8709981d
tioz2021 <jenya25life@gmail.com>
2023-03-09 14:21:44 +0200

    demo class

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,21 +1,29 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    int _num = 15;
-    std::cout << _num;
+class Person {
+public:
+    // Constructor
+    Person(std::string name, int age) {
+        m_name = name;
+        m_age = age;
+    }
+
+    // Member function
+    void say_hello() {
+        std::cout << "Hello, my name is " << m_name << " and I am " << m_age << " years old." << std::endl;
+    }
+
+private:
+    std::string m_name;
+    int m_age;
+};
+
+int main() {
+    // Create an instance of the Person class
+    Person john("Jenya", 25);
+
+    // Call the "say_hello" function on the Person object
+    john.say_hello();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
