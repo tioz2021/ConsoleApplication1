627807efbcd0d8aab021d6baf0d2851e0daf17fb
tioz2021 <jenya25life@gmail.com>
2023-03-09 17:28:32 +0200

    my base info for c++

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 int main() {
-    std::vector<int> arr;
+    vector<int> arr;
 
     for (int i = 1; i <= 5; i++) {
         arr.push_back(i);
     }
 
     for (int i = 0; i < arr.size(); i++) {
-        std::cout << arr[i] << "\n";
+        cout << arr[i] << "\n";
     }
+
+    // base types
+    int test = 10;
+    long testl = 10000;
+    float testF = 10;
+    char c = 'c';
+    bool b = false;
+
+    // array
+    int arr[3] = { 1,2,3 };
+    vector<int> d_arr; // dynamic array need add #include <vector>
+
+		// array method 
+		d_arr.push_back(10);
+		d_arr.size();
+
+    // string
+    string str = "string";
+
+    // flow conroll
+    if (true) {
+
+    }
+    else if(false) {
+
+    }
+    else {
+
+    }
+
+    while (true) {
+
+    }
+
+    do {
+
+    } while (true);
+
+    for (int i = 0; i < 10; i++) {
+
+    }
+
+
 
     return 0;
 }
